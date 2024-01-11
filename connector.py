90cc19fade333e926c6d6ba8250bf039a9ae34ea
“Binod <23531binod.2021@gmail.com>
2024-01-11 21:49:50 +0530

    updated code

--- connector.py
+++ connector.py
@@ -1,5 +1,5 @@
 import mysql.connector
-conn=mysql.connector.connect(host='localhost',username='root',password='Binod@1234',database='bagar')
+conn=mysql.connector.connect(host='localhost',username='root',password='Binod@xxxx',database='bagar')
 my_curser=conn.cursor()
 conn.commit()
 conn.close()
